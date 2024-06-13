119b4c35a5cd475980184092d56e3042c0388bf6
André Lima <dev@openkit.com.br>
2024-06-13 11:13:01 -0300

    Primeiro Commit - DevOpenkit

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Open-Kit</title>
+    <link rel="stylesheet" href="./assets/css/style.css">
+    <link rel="icon" type="image/svg+xml" href="./assets/img/fav.svg">
+</head>
+<body>
+    <nav id="menuPrincipal">
+        <img class="logoDaAplicação" src="./assets/img/logo.png" alt="Logotipo">
+        <ul>
+            <a class="linkMenuPrincipal" href="">🏠 Principal</a><br><br>
+            <a class="linkMenuPrincipal" href="">📝 Novo Pedido</a><br><br>
+            <a class="linkMenuPrincipal" href="">🔎 Consultar Clientes</a><br><br>
+            <a class="linkMenuPrincipal" href="">🔎 Consultar Pedidos</a><br><br>
+            <a class="linkMenuPrincipal" href="">🔎 Consultar Produtos</a><br><br>
+            <a class="linkMenuPrincipal" href="">📑 Relatórios Padrões</a><br><br>
+            <a class="linkMenuPrincipal" href="">⚙️ Administração</a><br><br>
+        </ul>
+    </nav>
+
+    <?php
+    
+    ?>
+
+
+    <div id="navTopHome">
+        <h3 style="color:#4D4D4E; margin-right: 15px; font-family:Arial, Helvetica, sans-serif; margin-left:260px;">Gestão de Pedidos</h3>
+            <div id="AreaIdentificaUsuarioHome">
+            <div style="display:flex;flex-direction:column;">
+            <h4 style="color:#4D4D4E; margin-right: 15px; font-family:Arial, Helvetica, sans-serif;">Bem Vindo;</h4><p style="color:#A6B4C6;font-family:Arial, Helvetica, sans-serif; font-size:14px;">Andrelino</p></div>
+            <img class="FotoPerfil" src="./assets/img/perfil.png" alt=""></div>
+            </div>
+
+    <div id="ContainerHome">
+  
+
+        <div id="boxHome1"></div>
+            <div id="boxHome2"></div>
+                <div id="boxHome3"></div>
+                    <div id="boxHome4"></div>
+    </div>
+
+ 
+
+    <script src="./assets/js/script.js"></script>
+</body>
+</html>- 
